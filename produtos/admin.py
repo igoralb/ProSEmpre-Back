7b3ebdabb5a5b9c9d56313790e027bb50172a2dd
João Victor do Nascimento Ribeiro de Andrade <nascimentojv01@gmail.com>
2024-10-16 21:21:14 -0300

    Dinamização da index.html

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Equipe
+from .models import LegendaBanner, NossasFerramentas, Artigos, PlataformaVideos, MateriaisApoio, PlataformaJogos, Equipe
 
-@admin.register(Equipe)
-class EquipeAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'profissao', 'registro_profissional']
+
+admin.site.register(Equipe)
+admin.site.register(LegendaBanner)
+admin.site.register(NossasFerramentas)
+admin.site.register(Artigos)
+admin.site.register(PlataformaVideos)
+admin.site.register(MateriaisApoio)
+admin.site.register(PlataformaJogos)
--- produtos/urls.py
+++ produtos/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
   
 urlpatterns = [
     path('', views.index),
+    path('videos/', views.videos_view, name='videos'),
+    path('forum/', views.forum_view, name='forum'),
+    path('pais_profs/', views.pais_profs_view, name='pais_profs'),
+
 ]
